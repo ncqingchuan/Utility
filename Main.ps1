82857e0eae3342060a6a6e9dae8e418451dc3d12
cqing <Chuan.Qing@activenetwork.com>
2023-02-17 13:48:32 +0800

    fix bug

--- Main.ps1
+++ Main.ps1
@@ -2,8 +2,8 @@ using Module .\Thread\CustomThreadPool.psm1
 
 Import-Module -Force -Name .\Data\Datasource.psm1
 $path = [System.IO.Path]::Combine($PSScriptRoot, "Data", "Datasource.psm1")
-$connectionString = "Data Source=192.168.0.4;Initial Catalog=master;UID=sa;PWD=;max pool size=5"
-$sql = "SELECT * FROM sys.objects where object_id=@objectId"
+$connectionString = 'Data Source=qingchuan;Initial Catalog=master;UID=sa;PWD=;max pool size=3'
+$sql = "SELECT @@SPID AS SPID ,@objectId as ObjectId"
 
 if ($PSVersionTable.PSEdition -eq "Core") {
     $lib = [System.IO.Path]::Combine($PSScriptRoot, "lib", "System.Data.SqlClient.dll")
@@ -30,7 +30,7 @@ $script = {
     try {
         Import-Module -Name $path -Force
         $connection = Get-DbConnection -connectionString $connectionString -providerFile $lib
-        $p1 = Get-NewParameter -parameterName "objectId" -value $objectId -dbType Int32
+        $p1 = Get-DbParameter -parameterName "objectId" -value $objectId -dbType Int32
         Get-ExecuteReader -connection $connection -commandText $sql -parameters $p1
     }
     catch {
@@ -38,11 +38,11 @@ $script = {
     }  
 }
 
-[CustomThreadPool]$pool = [CustomThreadPool]::new(3, 3, $Host)
+[CustomThreadPool]$pool = [CustomThreadPool]::new(3, 6, $Host)
 $jobs = @()
-1..30 | ForEach-Object { 
+1..6 | ForEach-Object { 
     $parameters.objectId = $_
-    $jobs += $pool.BeginInvoke( $script, $parameters )
+    $jobs += $pool.BeginInvoke( $script, $parameters)
 }
 
-$pool.EndInvoke($jobs).Table0 | Format-Table
+$pool.EndInvoke($jobs).List0
