b03211f10498859ba3e431c3554670bbe2449859
ncqingchuan <ncqingchuan@126.com>
2023-03-15 22:51:33 +0800

    add test case

--- Main.ps1
+++ Main.ps1
@@ -1,20 +1,23 @@
 using Module .\Thread\CustomThreadPool.psm1
 
 $scriptPath = "$($PSScriptRoot)\DataBase.ps1"
-$modules = "$($PSScriptRoot)/Thread/LockObject.psm1", "$($PSScriptRoot)\Data\Datasource.psm1"
+
 $parameters = @{
-    debug    = $true;
-    objectId = 0
+    objectId         = 0;
+    lock             = "$($PSScriptRoot)\TestResult\log.txt";
+    connectionString = 'Data Source=qingchuan;Initial Catalog=HighwaveDw;Integrated Security=true'
 }
 
-$jobs = @()
+$modules = "$($PSScriptRoot)\Thread\LockObject.psm1", "$($PSScriptRoot)\Data\Datasource.psm1"
+
 [CustomInitialSession]$session = [CustomInitialSession]::new()
 $session = $session.AddModules($modules)
+[CustomThreadPool]$pool = [CustomThreadPool]::new(10, 15, $session, $Host)
 
-[CustomThreadPool]$pool = [CustomThreadPool]::new(5, 5, $session, $Host)
-
-1..30 | ForEach-Object {
+$jobs = @()
+1..100 | ForEach-Object {
     $parameters.objectId = $_
     $jobs += $pool.BeginInvoke( $scriptPath, $parameters)
 }
-$pool.EndInvoke($jobs).Data.List0
+$pool.EndInvoke($jobs)
+
